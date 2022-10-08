5a9ad13eed30c14c6262e4921c7ee192ae4db7fc
Roberto Jimenez <robertojimenezg@gmail.com>
2022-10-08 00:39:28 -0500

    Ejemplo 4: Entendiendo el uso de props como props.children

--- task-proy/src/App.js
+++ task-proy/src/App.js
@@ -5,6 +5,7 @@ import ComponentA from './components/container/componentA';
 import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3.jsx';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -17,7 +18,13 @@ function App() {
         {/* Ejemplo de uso de Hooks */}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
-        <MiComponenteConContexto></MiComponenteConContexto>
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre = "Roberto">
+          {/** Todo lo que hay aquí adentro es tratado como props.children */}
+          <span>
+            Contenido del props.children
+          </span>
+        </Ejemplo4>
       </header>
     </div>
   );
